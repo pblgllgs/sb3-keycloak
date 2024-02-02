c8d9b8c8fcb5ac51f7355ba71acf77f9410cf6ef
pblgllgs <pbl.gllgs@gmail.com>
2024-02-02 12:12:28 -0300

    rest-client -> web-client

--- photoappwebclient/src/main/java/com/pblgllgs/photoappwebclient/PhotoappwebclientApplication.java
+++ photoappwebclient/src/main/java/com/pblgllgs/photoappwebclient/PhotoappwebclientApplication.java
@@ -3,18 +3,36 @@ package com.pblgllgs.photoappwebclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class PhotoappwebclientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhotoappwebclientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappwebclientApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient webClient(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository
+    ) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oAuth2AuthorizedClientExchangeFilterFunction =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+                        clientRegistrationRepository,
+                        oAuth2AuthorizedClientRepository
+                );
+        oAuth2AuthorizedClientExchangeFilterFunction.setDefaultOAuth2AuthorizedClient(true);
+        return WebClient.builder().apply(oAuth2AuthorizedClientExchangeFilterFunction.oauth2Configuration()).build();
+    }
 
 }
--- photoappwebclient/src/main/java/com/pblgllgs/photoappwebclient/controller/AlbumsController.java
+++ photoappwebclient/src/main/java/com/pblgllgs/photoappwebclient/controller/AlbumsController.java
@@ -4,22 +4,13 @@ import com.pblgllgs.photoappwebclient.response.AlbumRest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -37,36 +28,22 @@ public class AlbumsController {
     OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
 
     @Autowired
-    RestTemplate restTemplate;
+    WebClient webClient;
 
     @GetMapping("/albums")
     public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-
-        OAuth2AuthorizedClient oAuth2AuthorizedClient =
-                oAuth2AuthorizedClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), oauthToken.getName());
-        String jwtAccessToken = oAuth2AuthorizedClient.getAccessToken().getTokenValue();
-        log.info("accessToken: " + jwtAccessToken);
-
         log.info("Principal: " + principal);
 
-        OidcIdToken idToken = principal.getIdToken();
-        String idTokenValue = idToken.getTokenValue();
-        log.info("id token: " + idTokenValue);
-
         String url = "http://localhost:8082/albums";
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + jwtAccessToken);
-
-        HttpEntity<List<AlbumRest>> entity = new HttpEntity<>(headers);
 
 
-        ResponseEntity<List<AlbumRest>> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<List<AlbumRest>>() {
-        });
-
-        List<AlbumRest> albums = responseEntity.getBody();
+        List<AlbumRest> albums = webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<AlbumRest>>() {
+                })
+                .block();
 
         model.addAttribute("albums", albums);
         return "albums";
